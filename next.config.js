9d3c030593520cb58891a9f183adddd405b4ab6f
bezeeta222 <idham@thuleen.io>
2023-05-09 15:44:09 +0800

    initial

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+
+const withTM = require('next-transpile-modules')([
+  '@fullcalendar/common',
+  '@babel/preset-react',
+  '@fullcalendar/common',
+  '@fullcalendar/daygrid',
+  '@fullcalendar/interaction',
+  '@fullcalendar/react',
+  '@fullcalendar/timegrid',
+  '@fullcalendar/list',
+  '@fullcalendar/timeline'
+]);
+
+const nextConfig = withTM({
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  basePath: '/berry-dashboard',
+  assetPrefix: '/berry-dashboard/',
+});
+
+module.exports = nextConfig;
